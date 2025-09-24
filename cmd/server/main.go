ec007d0c2d0fc8ed21e1a6fde96a9d2c8d0989a9
thayour02 <you@example.com>
2025-09-24 16:55:23 +0300

    cici

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,7 +5,7 @@ import (
 	"database/sql"
 	"log"
 	"github.com/sava/env"
-
+	// "fmt"
 	_ "github.com/lib/pq"
 	"github.com/sava/cmd/api"
 	db "github.com/sava/db/sqlc"
@@ -14,10 +14,16 @@ import (
 	
 	var (
 		address = ":8080"
-	dbDriver = "postgres"
-	Dbsource string
+	   dbDriver = "postgres"
 
 )
+
+var dbSource = func() string {
+    if src := env.Getenv("DB_SOURCE", ""); src != "" {
+        return src
+    }
+    return "postgresql://root:secret@localhost:5434/order?sslmode=disable"
+}()
 
 func init() {
 	Dbsource :=  env.Getenv("DB_SOURCE", "dbsource")
@@ -25,9 +31,8 @@ func init() {
 		Dbsource = "postgresql://root:secret@localhost:5434/order?sslmode=disable" // fallback for local
 	}
 }
-
 func main() {
-	conn, err := sql.Open(dbDriver, Dbsource)
+	conn, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -5,25 +5,35 @@ import (
 	"log"
 	"os"
 	"testing"
-	"github.com/sava/env"
+
 	_ "github.com/lib/pq"
+	"github.com/sava/env"
 )
 
 
 var testQueries *Queries
-var (
-	dbDriver = "postgres"
-	Dbsource string
-)
+// 	const (dbDriver = "postgres")
 
-func init() {
-	Dbsource :=  env.Getenv("DB_SOURCE", "dbsource")
-	if Dbsource == "" {
-		Dbsource = "postgresql://root:secret@localhost:5434/order?sslmode=disable" // fallback for local
-	}
-}
+// 	var dbSource string
+
+// func init () {
+// 	dbSource = env.Getenv("DB_SOURCE", "dbsource")
+// 	if dbSource == "" {
+// 		dbSource = "postgresql://root:secret@localhost:5434/order?sslmode=disable" // fallback for local
+// 	}
+// }
+
+var dbDriver = "postgres"
+var dbSource = func() string {
+    if src := env.Getenv("DB_SOURCE", ""); src != "" {
+        return src
+    }
+    return "postgresql://root:secret@localhost:5434/order?sslmode=disable"
+}()
+
+
 func TestMain(m *testing.M) {
-	com, err := sql.Open(dbDriver, Dbsource)
+	com, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
