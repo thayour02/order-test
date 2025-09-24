300229c088ec26254e25c83563114d994dd477a0
thayour02 <you@example.com>
2025-09-24 15:45:59 +0300

    test ci

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,5 @@
 package main
 
-
 import (
 	"database/sql"
 	"log"
@@ -8,16 +7,26 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/sava/cmd/api"
 	db "github.com/sava/db/sqlc"
+	"github.com/sava/env"
 )
 
-const (
+var (
 	address = ":8080"
 	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5434/order?sslmode=disable"
+	DbSource string
 )
 
+func init() {
+	DbSource = env.Getenv("DB_SOURCE", "dbsource")
+
+	// fallback for local development
+	if DbSource == "" {
+		DbSource = "postgresql://root:secret@localhost:5434/order?sslmode=disable"
+	}
+}
+
 func main() {
-conn, err := sql.Open(dbDriver, dbSource)
+	conn, err := sql.Open(dbDriver, DbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
